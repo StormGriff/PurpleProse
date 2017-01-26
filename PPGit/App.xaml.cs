51161afba3752f7227f404c67a7e8fffcfe47cce
Nick <NRG626@yahoo.com>
2017-01-26 03:11:19 -0800

    Resource Fix
    Fix for issues in setup commit, with absolute paths on resources

---
+++ PPGit/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PurpleProse
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+        }
+
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.0.0.0")]
+        public static void Main()
+        {
+            PurpleProse.App app = new PurpleProse.App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+    }
+}
