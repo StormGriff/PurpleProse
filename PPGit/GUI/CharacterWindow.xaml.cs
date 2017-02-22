30480aede51710d2f5681e742d6e4357bf1625b4
Caston Young <Castony.pa@gmail.com>
2017-02-22 04:31:14 -0800

    About to implement listview

--- PPGit/GUI/CharacterWindow.xaml.cs
+++ PPGit/GUI/CharacterWindow.xaml.cs
@@ -31,6 +31,30 @@ namespace PurpleProse
 			this.Person = person;
 		}
 
+        #region Title Bar and Border
+
+        private void TitleBarText_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MaxButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = (this.WindowState == WindowState.Maximized) ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void MinButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        #endregion
+
 		private void DescBtn_Click(object sender, RoutedEventArgs e)
 			{ TextOps.Open(Person.DescFile); }
 
@@ -38,26 +62,27 @@ namespace PurpleProse
 	//	private void NameBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e) 
 	//		{ if (!it) it = true; }
 		
-		//Bug: Functions execute upon inital click
-		private void NameBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+		//Bug: Functions execute upon inital click. (Originally named XXX_LostKeyboardFocus)
+		//ComboBox Tutorial: https://youtu.be/UDDYd3q5WM4
+		private void NameBox_LostKeyFocus(object sender, KeyboardFocusChangedEventArgs e)
 			{ if (NameBox.Text != "") Person.Name = NameBox.Text; } 
 
-		private void AgeBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e) 
+		private void AgeBox_LostKeyFocus(object sender, KeyboardFocusChangedEventArgs e) 
 			{ if (AgeBox.Text != "") Person.charAge = 5; }	//Some sort of atoi needs to be implimented
 
-		private void GenderBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e) 
-			{ if (GenderBox.Text != "") Person.Name = GenderBox.Text; }
+		private void GenderBox_LostKeyFocus(object sender, KeyboardFocusChangedEventArgs e) 
+			{ if (GenderBox.Text != "") Person.charGender = GenderBox.Text; }
 
-		private void HomeBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e) 
+		private void HomeBox_LostKeyFocus(object sender, KeyboardFocusChangedEventArgs e) 
 			{ if (HomeBox.Text != "") Person.charHometown = HomeBox.Text; }
 
-		private void RoleBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+		private void RoleBox_LostKeyFocus(object sender, KeyboardFocusChangedEventArgs e)
 			{ if (RoleBox.Text != "") Person.charRole = RoleBox.Text; }
 
-		private void LingBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e) 
+		private void LingBox_LostKeyFocus(object sender, KeyboardFocusChangedEventArgs e) 
 			{ if (LingBox.Text != "") Person.charLanguage = LingBox.Text; }
 
-		private void RaceBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e) 
+		private void RaceBox_LostKeyFocus(object sender, KeyboardFocusChangedEventArgs e) 
 			{ if (RaceBox.Text != "") Person.charKind = RaceBox.Text; }
-	}
+    }
 }
--- PPGit/GUI/MainWindow.xaml.cs
+++ PPGit/GUI/MainWindow.xaml.cs
@@ -54,28 +54,58 @@ namespace PurpleProse
         {
             characters.Add(new PurpleProse.Lib.Character("bob", null, null, null, 20, null, null, null, null, null, null));
             characters.Add(new PurpleProse.Lib.Character("Jimbo", null, null, null, 15, null, null, null, null, null, null));
-
-            
-
-            DataColumn dc = new DataColumn();
-            dc.ColumnName = "Name";
-            dc.DataType = System.Type.GetType("System.String");
-            bindChar.Columns.Add(dc);
-            dc = new DataColumn();
-            dc.ColumnName = "Age";
-            dc.DataType = System.Type.GetType("System.Int64");
-            bindChar.Columns.Add(dc);
-
+			/*
+			NewColumn("Name"); NewColumn("Age", "System.Int64"); NewColumn(new List<string> { "Gender", "Race", "Role", "hometown", "language" });
             //bindChar.Columns.Add(new DataColumn("Name"));
             //bindChar.Columns.Add(new DataColumn("Age"));
 
             UpdateBinding();
 
             DataGrid.DataContext = bindChar.DefaultView;
-			
-			/*texteditor*/ TextOps.editor = "D:\\Documents\\Visual Studio 2015\\Projects\\HelloWPF\\HelloWPF\\bin\\Debug\\HelloWPF.exe";//"D:\\Documents\\Visual Studio 2015\\Projects\\PurpleProse\\PPGit\\Resources\\HelloWPF - Shortcut.exe";
+			*/
+
+			TextOps.editor = "D:\\Documents\\Visual Studio 2015\\Projects\\HelloWPF\\HelloWPF\\bin\\Debug\\HelloWPF.exe";//"D:\\Documents\\Visual Studio 2015\\Projects\\PurpleProse\\PPGit\\Resources\\HelloWPF - Shortcut.exe";
 			// Intialization of default texteditor must be moved, and edited.
         }
+
+		private void Window_Activated(object sender, System.EventArgs e)
+		{if(this.IsLoaded){
+				//UpdateBinding();
+		}}
+
+		private void Elements_Loaded(object sender, RoutedEventArgs e)
+		{
+			// ... Create a TreeViewItem.
+			TreeViewItem _Characters = new TreeViewItem();
+			_Characters.Header = "Characters";
+			_Characters.ItemsSource = new Lib.Character[] { new Lib.Character("bob", null, null, null, 20, null, null, null, null, null, null) };
+
+			// ... Create a second TreeViewItem.
+			TreeViewItem _Locations = new TreeViewItem();
+			_Locations.Header = "Locations";
+			_Locations.ItemsSource = new string[] { "Pants", "Shirt", "Hat", "Socks" };
+			
+			Elements.Items.Add(_Characters);
+			Elements.Items.Add(_Locations);
+
+			//Elements.Items[0].Add(new Lib.Character("Jimbo", null, null, null, 15, null, null, null, null, null, null));
+
+		}
+
+		private void Elements_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+		{
+			var tree = sender as TreeView;
+
+			// ... Determine type of SelectedItem.
+			if (tree.SelectedItem is TreeViewItem)
+			{	var item = tree.SelectedItem as TreeViewItem;
+				
+			}
+			else if (tree.SelectedItem is string)
+			{
+			}
+			
+		}
 
 		private void MenuItem_Click(object sender, RoutedEventArgs e) {
 			(sender as MenuItem).ContextMenu.IsEnabled = true;
@@ -90,21 +120,43 @@ namespace PurpleProse
             bindChar.Clear();
 
             foreach(PurpleProse.Lib.Character c in characters)
-            {
-                DataRow dr = bindChar.NewRow();
-                dr["Name"] = c.Name;
-                dr["Age"] = c.charAge;
+            {	DataRow dr = bindChar.NewRow();
+                dr["Name"	 ] = c.Name;
+                dr["Age"	 ] = c.charAge;
+				dr["Race"	 ] = c.charKind;
+				dr["Gender"	 ] = c.charGender;
+				dr["Role"	 ] = c.charRole;
+				dr["Language"] = c.charLanguage;
+				dr["Hometown"] = c.charHometown;
                 bindChar.Rows.Add(dr);
             }
         }
+
+		private void NewColumn(string label, string ctype = "System.String")
+		{	DataColumn dc = new DataColumn();
+			dc.ColumnName = label;
+			dc.DataType = System.Type.GetType(ctype);
+			bindChar.Columns.Add(dc);
+		}
+		private void NewColumn(List<string> labels)
+		{	DataColumn dc;
+			foreach(string label in labels)
+			{	dc = new DataColumn();
+				dc.ColumnName = label;
+				dc.DataType = System.Type.GetType("System.String");
+				bindChar.Columns.Add(dc);
+			}
+		}
 
 		private void Add_Character_LeftMouseUp(object sender, MouseButtonEventArgs e) {
 			PurpleProse.Lib.Character New_Char = new PurpleProse.Lib.Character(null, null, null, null, 20, null, null, null, null, null, null);
 			characters.Add(New_Char);
 			CharacterWindow CharWindow = new CharacterWindow(New_Char);
 			CharWindow.Show();
-			//MessageBox.Show(CharWindow.Getmessage());
-			//CharWindow.ShowDialog();
+			//Elements.Items[0]; //Add("Text");
+			var sub_folder = Elements.Items[0] as TreeViewItem;
+			sub_folder.Items.Add(New_Char);
 		}
+
 	}
 }
