08ff89fb11d9356aa130d660ea85d94f9d99fd65
Stash5000 <stash5000@gmail.com>
2017-04-05 08:42:25 -0700

    Added Recycle as a singleton class

---
+++ PPGit/Lib/recycle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPGit.Lib
+{
+    public class recycle
+    {
+        private static recycle instance = null;
+        private const int DAYS_TO_DELETE = 10;
+        public struct item {
+            public PurpleProse.Lib.Object myObject;
+            public DateTime delete;
+        }
+        private List<item> bin;
+        private recycle() {
+            bin = new List<item>();
+        }
+
+        //Get single instance of recycle
+        public static recycle Bin {
+            get {
+                if (instance == null) {
+                    instance = new recycle();
+                }
+                return instance;
+            }
+        }
+        public void add(List<PurpleProse.Lib.Object> myList) {
+            foreach (PurpleProse.Lib.Object theOb in myList) {
+                item adding = new item();
+                adding.myObject = theOb;
+                adding.delete = DateTime.Now.AddDays(DAYS_TO_DELETE);
+                bin.Add(adding);
+            }
+        }
+
+        public void add(List<item> itemList) {
+            foreach (item theItem in itemList) {
+                bin.Add(theItem);
+            }
+        }
+
+        public void add(PurpleProse.Lib.Object singleObject) {
+            item newItem = new item();
+            newItem.myObject = singleObject;
+            newItem.delete = DateTime.Now.AddDays(DAYS_TO_DELETE);
+        }
+
+        //Delete all items older than 10 days
+        private void clean() {
+            foreach (item list in bin) {
+                if (DateTime.Now > list.delete) bin.Remove(list);
+            }
+        }
+
+        public List<item> getList {
+            get {
+                clean();
+                return bin;
+            }
+        }
+    }
+}
