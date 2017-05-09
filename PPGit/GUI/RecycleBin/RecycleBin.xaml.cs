0db454d5ab02bb5c8252b2407c998c2f135ed09c
Nick <NRG626@yahoo.com>
2017-05-08 21:27:59 -0700

    Merge for recycle bin
    -added theming
    -organized files

---
+++ PPGit/GUI/RecycleBin/RecycleBin.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+
+using MahApps.Metro.Controls;
+
+namespace PPGit.GUI.RecycleBin
+{
+    /// <summary>
+    /// Interaction logic for RecycleBin.xaml
+    /// </summary>
+    public partial class RecycleBin : MetroWindow
+    {
+        DataTable newTable;
+        public RecycleBin()
+        {
+            newTable = new DataTable();
+            InitializeComponent();
+        }
+
+        private void recycleFRM_Loaded(object sender, RoutedEventArgs e)
+        {
+            /*DataColumn newCol = new DataColumn();
+            newCol.ColumnName = "Name"; //Column 1
+            newCol.DataType = System.Type.GetType("System.String");
+            newTable.Columns.Add(newCol);
+            DataColumn newCol2 = new DataColumn();
+            newCol2.ColumnName = "Deletion in... (days)";
+            newCol2.DataType = System.Type.GetType("System.Int64");
+            newTable.Columns.Add(newCol2);*/
+            newTable.Columns.Add("Name", typeof(string));
+            newTable.Columns.Add("Deleted", typeof(string));
+            List<PPGit.Lib.Recycle.item> theList = PPGit.Lib.Recycle.Bin.getList;
+            foreach (PPGit.Lib.Recycle.item thisOb in theList)
+            {
+                TimeSpan newSpan = thisOb.delete - DateTime.Now;
+                DataRow newRow = newTable.NewRow();
+                newRow["Name"] = thisOb.myObject.Name;
+                newRow["Deleted"] = Math.Ceiling(newSpan.TotalDays).ToString() + " days";
+                newTable.Rows.Add(newRow);
+                //newTable.Rows.Add(thisOb.myObject.Name, newSpan.Days);
+            }
+            binLST.ItemsSource = newTable.DefaultView;
+        }
+
+        private void restoreBTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (binLST.SelectedItem != null)
+            {
+                int num = binLST.SelectedIndex;
+                string find = newTable.Rows[num][0].ToString();
+                PPGit.Lib.Recycle.Bin.restore(find);
+                newTable.Rows.Remove(newTable.Rows[num]);
+            }
+        }
+
+        private void btnRightWindowChangeTheme_Click(object sender, RoutedEventArgs e)
+        {
+            AppThemeChanger wnd = new AppThemeChanger();
+
+            wnd.Show();
+        }
+    }
+}
---
+++ PPGit/mainLists.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPGit
+{
+    public static class mainLists
+    {
+        public static List<PPGit.Lib.Character> characterList = new List<PPGit.Lib.Character>();
+        public static List<PPGit.Lib.Location> locationList = new List<PPGit.Lib.Location>();
+    }
+}
